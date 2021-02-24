a290c48fd9fffbc8d791a82b1f43460898026806
Ma3tin <martin.mares@educanet.cz>
2021-02-24 14:16:45 +0100

    horiznotal

--- src/main/java/cz/educanet/tranformations/logic/ScreenManager.java
+++ src/main/java/cz/educanet/tranformations/logic/ScreenManager.java
@@ -26,79 +26,34 @@ public class ScreenManager {
         return selectedPoints;
     }
 
-    public boolean isFilledIn(Coordinate coordinate) { // TODO: Implement this
-        ArrayList<Coordinate> coordinatesOfSelectedPoints = new ArrayList<>(selectedPoints);
-        if (coordinatesOfSelectedPoints.size() == 3) {
-            double coordinateAX = coordinatesOfSelectedPoints.get(0).getX();
-            double coordinateAY = coordinatesOfSelectedPoints.get(0).getY();
-            double coordinateBX = coordinatesOfSelectedPoints.get(1).getX();
-            double coordinateBY = coordinatesOfSelectedPoints.get(1).getY();
-            double coordinateCX = coordinatesOfSelectedPoints.get(2).getX();
-            double coordinateCY = coordinatesOfSelectedPoints.get(2).getY();
-
-            double a1 = (coordinateAY - coordinateBY) / (coordinateAX - coordinateBX);
-            double a2 = (coordinateBY - coordinateCY) / (coordinateBX - coordinateCX);
-            double a3 = (coordinateCY - coordinateAY) / (coordinateCX - coordinateAX);
-
-            double b1 = coordinateAY - (a1 * coordinateAX);
-            double b2 = coordinateBY - (a2 * coordinateBX);
-            double b3 = coordinateCY - (a3 * coordinateCX);
+    public boolean isFilledIn(Coordinate p) {
+        ArrayList<Coordinate> selected = new ArrayList<>(selectedPoints);
+        if (selected.size() == 3) {
+            Coordinate pA = selected.get(0);
+            Coordinate pB = selected.get(1);
+            Coordinate pC = selected.get(2);
 
 
-            System.out.println("a1=" + a1 + " b1=" + b1 );
-            System.out.println("a2=" + a2 + " b2=" + b2 );
-            System.out.println("a3=" + a3 + " b3=" + b3 );
-            boolean first = false;
-            boolean second = false;
-            boolean third = false;
-
-            if (coordinateCY > a1 * coordinateCX + b1) {
-                if (coordinate.getY() > a1 * coordinate.getX() + b1)
-                    first = true;
-                else
-                    first = false;
-            }
-
-            if (coordinateCY < a1 * coordinateCX + b1) {
-                if (coordinate.getY() < a1 * coordinate.getX() + b1)
-                    first = true;
-                else
-                    first = false;
-            }
-
-
-            if (coordinateAY > a2 * coordinateAX + b2) {
-                if (coordinate.getY() > a2 * coordinate.getX() + b2)
-                    second = true;
-                else
-                    second = false;
-            }
-
-            if (coordinateAY < a2 * coordinateAX + b2) {
-                if (coordinate.getY() < a2 * coordinate.getX() + b2)
-                    second = true;
-                else
-                    second = false;
-            }
-
-
-            if (coordinateBY > a3 * coordinateBX + b3) {
-                if (coordinate.getY() > a3 * coordinate.getX() + b3)
-                    third = true;
-                else
-                    third = false;
-            }
-
-            if (coordinateBY < a3 * coordinateBX + b3) {
-                if (coordinate.getY() < a3 * coordinate.getX() + b3)
-                    third = true;
-                else
-                    third = false;
-            }
-            if (first && second && third)
+            if (isInSameHalfPlane(pA, pB, pC, p)
+                    && isInSameHalfPlane(pB, pC, pA, p)
+                    && isInSameHalfPlane(pC, pA, pB, p))
                 return true;
         }
 
         return false;
+    }
+
+    public boolean isInSameHalfPlane(Coordinate pA, Coordinate pB, Coordinate pC, Coordinate p) {
+        if (pA.getX() == pB.getX()) { //vertical
+            return (pC.getX() > pA.getX() && p.getX() > pA.getX()) || (pC.getX() < pA.getX() && p.getX() < pA.getX());
+        } else { //otherwise
+            double ax = pA.getX(), ay = pA.getY();
+            double bx = pB.getX(), by = pB.getY();
+
+            double a = (ay - by) / (ax - bx);
+            double b = ay - (a * ax);
+
+            return ((pC.getY() > a * pC.getX() + b) && (p.getY() > a * p.getX() + b)) || ((pC.getY() < a * pC.getX() + b) && (p.getY() < a * p.getX() + b));
+        }
     }
 }
